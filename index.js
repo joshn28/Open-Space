3d144818c95cfff15c5002551b11c21e7a81db7c
joshn28 <jono2td8@gmail.com>
2021-04-29 15:13:35 -0400

    Lock the launch button until all the checkboxes are checked and all the input ranges are set to maximum.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function disable() {
-    let controls = document.querySelectorAll("input");
-    console.log(controls);
+    const controls = document.querySelectorAll("input");
     for (let i = 2; i < controls.length; i++) {
-        console.log(controls[i]);
         controls[i].setAttribute("disabled","");
+        controls[i].onchange = function() {check_maximum()};
     }
 }
 
@@ -12,9 +11,31 @@ window.addEventListener('load', function() {
         let password = document.getElementById("password").value;
         if (password === "TrustNo1") {
             let controls = document.querySelectorAll("input");
-            for (let i = 2; i < controls.length; i++) {
+            for (let i = 2; i < controls.length - 1; i++) {
                 controls[i].removeAttribute("disabled");
             }
         }
     })
+
+    document.getElementById("launch-button").addEventListener("click", function () {
+        console.log("BLAST OFF!");
+        window.moving = setInterval(launching, 50);
+    })
 })
+
+function check_maximum() {
+    let i = 2;
+    console.log("Checking if checkboxes are checked and all input ranges are set to maximum");
+    const controls = document.querySelectorAll("input");
+    for (i; i < 8; i++) {
+        if (!controls[i].checked) {
+            return;
+        }
+    }
+    for (i; i < controls.length - 1; i++) {
+        if (controls[i].value !== "100") {
+            return;
+        }
+    }
+    controls[i].removeAttribute("disabled");
+}
