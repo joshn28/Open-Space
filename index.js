50386476cca80c1609f7e1c5eded398662756ac7
joshn28 <jono2td8@gmail.com>
2021-04-29 12:19:09 -0400

    Enable disabled inputs when the correct password is entered.

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function disable() {
     }
 }
 
+window.addEventListener('load', function() {
+    document.getElementById("ok-button").addEventListener("click", function() {
+        let password = document.getElementById("password").value;
+        if (password === "TrustNo1") {
+            let controls = document.querySelectorAll("input");
+            for (let i = 2; i < controls.length; i++) {
+                controls[i].removeAttribute("disabled");
+            }
+        }
+    })
+})
